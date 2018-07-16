ebcfdbd9a123b0df19446e440c36aca2e0b96b0a
AJ Genung <DragonRydrz@me.com>
2018-07-16 14:38:02 -0400

    user/invoice models created, server runs

--- server/models/users.js
+++ server/models/users.js
@@ -4,50 +4,59 @@ const bcrypt = require('bcrypt');
 
 mongooseTypes.loadTypes(mongoose, 'email');
 
-const { Schema } = mongoose;
 const { Email } = mongoose.Schema.Types;
 const { ObjectId } = mongoose.Schema.Types;
 
 const SALT_ROUNDS = 11;
 
-const UserSchema = new Schema({
-  username: {
-    type: Email,
-    unique: true,
-    required: true,
+const User = new mongoose.Schema(
+  {
+    // E-mail address is used as a username
+    username: {
+      type: Email,
+      unique: true,
+      required: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: Number,
+      required: true,
+    },
+    subscription: {
+      type: String,
+      default: 'perdiem',
+    },
+    invoice: { type: ObjectId, ref: 'Invoice' },
   },
-  firstName: { 
-    type: String,
-    required: true,
-  },
-  lastName: {
-    type: String,
-    required: true, 
-  },
-  password: {
-    type: String,
-    required: true,
-  },
+  {
+    timestamps: true,
+  }
+);
 
-  },
-{
-  timestamps: true
-});
-
-
-UserSchema.pre('save', function (next) {
- if (this.password !== null) {
-   bcrypt.hash(this.password, SALT_ROUNDS, (err, hashed) => {
-     if (err) return next(err);
-     this.password = hashed;
-     next();
-   });
- }
-});
-
-UserSchema.methods.checkPassword = function (plainTextPW) {
+User.pre('save', function(next) {
   if (this.password !== null) {
-  return bcrypt.compare(plainTextPW, this.password);
+    bcrypt.hash(this.password, SALT_ROUNDS, (err, hashed) => {
+      if (err) return next(err);
+      this.password = hashed;
+      next();
+    });
+  }
+});
+
+User.methods.checkPassword = function(plainTextPW) {
+  if (this.password !== null) {
+    return bcrypt.compare(plainTextPW, this.password);
   }
 };
 
---
+++ server/server.js
@@ -0,0 +1,31 @@
+// const config = require('./secrets/config');
+const express = require('express');
+const mongoose = require('mongoose');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const server = express();
+server.use(morgan('dev'));
+server.use(express.json());
+server.use(helmet());
+server.use(cors());
+
+server.get('/', (req, res) => {
+  res.json({ message: 'API running successfully! YAY!' });
+});
+
+mongoose
+  .connect(process.env.DATA_BASE || 'mongodb://localhost/givememymoney')
+  .then(() => {
+    console.log('Connected to DB');
+  })
+  .catch(err => {
+    console.log('Error connecting to DB', err);
+  });
+
+const port = process.env.PORT || 5000;
+
+server.listen(port, err => {
+  console.log(`connected to the server port ${port}`);
+});
